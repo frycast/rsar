1c688a9df1bac36b1d3ddf5fa56af2ee1c76ebd4
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-08-11 20:17:26 +1000

    Four good functions

--- R/reshape_tif.R
+++ R/reshape_tif.R
@@ -1,54 +1,146 @@
-#' reshaper
+
+#' SAR_matrix
 #'
-#' @param br_a A raster brick
-#' @param m A matrix
+#' Construct a \code{SAR_matrix} object from a matrix \code{m}.
+#' Each column of a \code{SAR_matrix} corresponds to a band in
+#' a SAR raster brick (i.e., to the depth dimension),
+#' and each row corresponds to a pixel.
+#'
+#' @param m A matrix with raster intensities.
+#' @param extent An \code{\link[raster]{extent}} object.
+#' @param crs A string specifying the coordinate reference system
+#' (as in \code{\link[raster]{crs}}).
+#' @param brick_nrow A single integer specifying the number of rows
+#' (in pixels) of each band in the brick. Note that
+#' \code{brick_nrow * brick_nrow} must equal \code{nrow(m)}.
+#' The default value of 2 is for demonstration and
+#' is unlikely to be useful.
+#' @param brick_ncol A single integer specifying the number of
+#' columns (in pixels) of each band in the brick. Note that
+#' \code{brick_nrow * brick_nrow} must equal \code{nrow(m)}.
+#' @param brick_names A vector of length \code{ncol(m)}
+#' labelling each band in the raster \code{\link[raster]{brick}}.
 #'
 #' @return
+#' A \code{SAR_matrix} object; a specialisation
+#' of class matrix that includes geospatial
+#' and brick dimension attributes.
+#'
 #' @export
 #'
 #' @examples
-reshaper <- function(br_a, m) {
-  if (missing(m)) {
-    return(reticulate::array_reshape(br_a, dim = c(dim(br_a)[1L]*dim(br_a)[2L], dim(br_a)[3L])))
+#' SAR_matrix()
+#'
+SAR_matrix <- function(
+  m = matrix(0, 4, 3), extent = raster::extent(raster::raster()),
+  crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
+  brick_nrow = 2, brick_ncol = nrow(m) / brick_nrow,
+  brick_names = paste0( "layer.", 1:ncol(m) ) ) {
 
-  } else {
-    dim <- c(dim(br_a)[1L], dim(br_a)[2L], dim(br_a)[3L])
-    br_a2 <- reticulate::array_reshape(m, dim = dim)
-    br_b <- raster::brick(br_a2)
-    return(br_b)
-  }
+  assertthat::assert_that( brick_nrow * brick_ncol == nrow(m) )
+  assertthat::assert_that( length(brick_nrow) == 1 )
+  assertthat::assert_that( length(brick_ncol) == 1 )
+  assertthat::assert_that( length(brick_names) == ncol(m) )
+
+  m <- structure(
+    m, class = c("SAR_matrix", class(m)),
+    brick_extent = extent,
+    brick_crs = crs,
+    brick_dim = c( brick_nrow, brick_ncol, ncol(m) ),
+    brick_names = brick_names)
+  return(m)
 }
 
-#' give_attrs
+
+#' load_SAR_matrix
 #'
-#' @param br_b A raster brick
-#' @param br A raster brick
-#' @param incl_names Logical
+#' Given a file path to a SAR raster image
+#' in \code{tif} format, convert the raster brick of
+#' dimensions \code{A x B x C} into a matrix of
+#' dimensions \code{A*B x C} while keeping
+#' track of the geospatial and dimension attributes of the
+#' original \code{\link[raster]{brick}}.
+#'
+#' @param filename This is usually a string giving absolute or relative
+#' path to a \code{tif} file containing a SAR raster brick. This argument is
+#' passed to \code{\link[raster]{brick}}.
 #'
 #' @return
+#' An object of class \code{\link[rsar]{SAR_matrix}}; a specialisation
+#' of class matrix.
+#'
 #' @export
 #'
 #' @examples
-give_attrs <- function(br_b, br, incl_names = TRUE) {
-  raster::crs(br_b) <- raster::crs(br)
-  raster::extent(br_b) <- raster::extent(br)
-  if (incl_names) { names(br_b) <- names(br) }
-  return(br_b)
+#' filename <- system.file(
+#'   "extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
+#' load_SAR_matrix(filename)
+#'
+load_SAR_matrix <- function(filename) {
+  return(brick_to_matrix(raster::brick(filename)))
 }
 
-#' reshapekDL
+
+#' brick_to_matrix
 #'
-#' @param br_a A raster brick
-#' @param m A matrix
-#' @param k An integer
+#' Convert a raster \code{\link[raster]{brick}} of
+#' dimensions \code{A x B x C} into a matrix of
+#' dimensions \code{A*B x C} while keeping
+#' track of the geospatial and dimension attributes of the
+#' original brick.
+#'
+#' @param b A raster \code{\link[raster]{brick}}.
 #'
 #' @return
+#' An object of class \code{\link[rsar]{SAR_matrix}}; a specialisation
+#' of class matrix.
+#'
 #' @export
 #'
 #' @examples
-reshapekDL <- function(br_a, m, k = 2L) {
-  dim <- c(dim(br_a)[1L], dim(br_a)[2L], k)
-  br_a2 <- reticulate::array_reshape(m, dim = dim)
-  br_b <- raster::brick(br_a2)
-  return(br_b)
+#' filename <- system.file(
+#'   "extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
+#' b <- raster::brick(filename)
+#' m <- brick_to_matrix(b)
+#'
+brick_to_matrix <- function(b) {
+  b_dim <- dim(b)
+  d <- c(b_dim[1L]*b_dim[2L], b_dim[3L])
+  m <- reticulate::array_reshape(raster::as.array(b), dim = d)
+
+  brick_attr <- attributes(b)[c("extent", "crs")]
+  m <- structure(m, class = c("SAR_matrix", class(m)),
+                 brick_extent = brick_attr$extent,
+                 brick_crs = brick_attr$crs,
+                 brick_dim = b_dim,
+                 brick_names = names(b))
+  return(m)
+}
+
+
+#' matrix_to_brick
+#'
+#' Convert an \code{\link[rsar]{SAR_matrix}} object \code{m},
+#' such as that output by \code{\link[rsar]{load_SAR_matrix}}, into a
+#' raster \code{\link[raster]{brick}} with the
+#' geospatial and dimension attributes stored in \code{m}.
+#'
+#' @param m An \code{SAR_matrix}  object, such as that output
+#' by \code{\link[rsar]{load_SAR_matrix}}.
+#'
+#' @return
+#' A raster \code{\link[raster]{brick}}.
+#'
+#' @export
+#'
+#' @examples
+#' filename <- system.file(
+#'   "extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
+#' m <- load_SAR_matrix(filename)
+#' matrix_to_brick(m)
+#'
+matrix_to_brick <- function(m) {
+  dim <- attr(m, "brick_dim")
+  b <- raster::brick(reticulate::array_reshape(m, dim = dim))
+  return(b)
 }
---
+++ R/rsar.R
@@ -0,0 +1,17 @@
+#' Package title
+#'
+#' Details
+#'
+#'@section Update: Current updates are available through
+#' URL: https://github.com/frycast/rsar
+#'
+#'@section BugReports: https://github.com/frycast/rsar/issues
+#'
+#'@description Description
+#'
+#'@author Daniel Fryer \email{daniel-fryer@live.com.au},
+#'Mason Terrett \email{Mason.Terrett@gmail.com}
+#'@docType package
+#'@name rsar
+#'@aliases rsar-package
+NULL
