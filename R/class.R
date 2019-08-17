0b6cb084edaa30b0ed60f4818efd124242d6c439
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-08-17 17:32:03 +1000

    improve SAR_matrix constructor

--- R/class.R
+++ R/class.R
@@ -1,3 +1,7 @@
+
+##########################
+### WRITE TESTS (X)
+##########################
 #' SAR_matrix
 #'
 #' Construct a \code{SAR_matrix} object from a matrix \code{m}.
@@ -19,6 +23,10 @@
 #' \code{brick_nrow * brick_nrow} must equal \code{nrow(m)}.
 #' @param brick_names A vector of length \code{ncol(m)}
 #' labelling each band in the raster \code{\link[raster]{brick}}.
+#' @param attr_src Either another \code{SAR_matrix} or a
+#' raster \code{\link[raster]{brick}}. If this argument is not
+#' missing then all other arguments to this function will be sourced
+#' from \code{attr_src}, except for the argument \code{brick_names}.
 #'
 #' @return
 #' A \code{SAR_matrix} object; a specialisation
@@ -34,26 +42,36 @@ SAR_matrix <- function(
   m = matrix(0, 4, 3), extent = raster::extent(raster::raster()),
   crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
   brick_nrow = 2, brick_ncol = nrow(m) / brick_nrow,
-  brick_names = paste0( "layer.", 1:ncol(m) )) {
+  brick_names = paste0( "layer.", 1:ncol(m) ), attr_src) {
 
   assertthat::assert_that( brick_nrow * brick_ncol == nrow(m) )
   assertthat::assert_that( length(brick_nrow) == 1 )
   assertthat::assert_that( length(brick_ncol) == 1 )
   assertthat::assert_that( length(brick_names) == ncol(m) )
 
-  m <- structure(
-    m, class = c("SAR_matrix", class(m)),
-    extent = extent,
-    crs = crs,
-    brick_dim = c( brick_nrow, brick_ncol, ncol(m) ),
-    brick_names = brick_names)
+  if ( missing(attr_src) ) {
+    m <- structure(
+      m, class = c("SAR_matrix", class(m)),
+      extent = extent,
+      crs = crs,
+      brick_dim = c( brick_nrow, brick_ncol, ncol(m) ),
+      brick_names = brick_names)
+  } else {
+    m <- SAR_matrix(
+      m,
+      extent = attr(attr_src, "extent"),
+      crs = attr(attr_src, "crs"),
+      brick_nrow = attr(attr_src, "brick_dim")[1],
+      brick_ncol = attr(attr_src, "brick_dim")[2])
+  }
   return(m)
 }
 
 
 
-
-
+##########################
+### WRITE TESTS (X)
+##########################
 #' is_SAR_matrix
 #'
 #' Check if an object is of class \code{\link[rsar]{SAR_matrix}}.
