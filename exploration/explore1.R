5bfdbcfd55adb6d20d7a9e510ba084c71ad7b14a
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-08-17 17:32:42 +1000

    exploration

--- exploration/explore1.R
+++ exploration/explore1.R
@@ -2,6 +2,6 @@ filename <- system.file(
    "extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
 b <- raster::brick(filename)
 
-
 m <- rsar::brick_to_matrix(b)
 b2 <- rsar::matrix_to_brick(m)
+
