4bef50e6b1cac6113c6eb4fd37bfa03008228d67
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-08-17 18:14:01 +1000

    create first AR time_series functions

---
+++ R/time_series.R
@@ -0,0 +1,62 @@
+#' choose_order
+#'
+#' Take a random sample (of size \code{sample_size})
+#' of the rows of \code{m}, then fit an AR model to
+#' each row in the sample, using
+#' AIC to determine the optimal order.
+#' Return the largest order across all sampled
+#' rows.
+#'
+#' @param m A matrix.
+#' @param sample_size An integer smaller than
+#' \code{nrow(m)}.
+#'
+#' @return
+#' An integer.
+#'
+#' @export
+#'
+#' @examples
+#' filepath <- system.file("extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
+#' m <- load_SAR_matrix(filepath)
+#' order <- choose_order(m)
+#'
+#'
+choose_order <- function(m, sample_size = 1e3L) {
+  s <- sample(1:nrow(m), sample_size)
+  ms <- m[s,]
+  max_or <- 0
+  for (i in 1:nrow(ms)) {
+    or <- ar(ms[i,])$order
+    if ( or > max_or ) {max_or <- or}
+  }
+  return(as.integer(max_or))
+}
+
+
+#' fit_AR_to_SAR
+#'
+#' Fit an AR model with the chosen \code{order}
+#' to each row of \code{m} using \code{\link[base]{ar}}
+#' (so each row of \code{m} is treated as a single
+#' time series). The results are returned in a matrix whose
+#' columns are the resulting AR coefficients.
+#'
+#' @param m A matrix.
+#'
+#' @return
+#' A matrix of AR coefficients where each row corresponds to
+#' one row of \code{m}.
+#'
+#' @export
+#'
+#' @examples
+#' filepath <- system.file("extdata", "MG_CC_sub_norm_testclip.tif", package="rsar")
+#' m <- load_SAR_matrix(filepath)
+#' ar_m <- fit_AR_to_SAR(m)
+#'
+fit_AR_to_SAR  <- function(m, order = choose_order(m)) {
+  ar_m <- t(apply(m, MARGIN = 1, function(x){ar(x, order = order, aic = F)$ar}))
+  colnames(ar_m) <- paste0("p", 1:order)
+  return(ar_m)
+}
