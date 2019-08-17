0e738a63f9bf9f17ac7314b360cca0a37418315b
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2019-08-17 13:16:13 +1000

    Attempt fix travis-CI build error

--- tests/testthat/test-reshape_tif.R
+++ tests/testthat/test-reshape_tif.R
@@ -3,19 +3,19 @@ filename_cc <- system.file(
 filename_vh <- system.file(
   "extdata", "MG_VH_sub_norm_testclip.tif", package="rsar")
 
-cc_m1 <- load_SAR_matrix(filename_cc)
-cc_b1 <- raster::brick(filename_cc)
-cc_m2 <- brick_to_matrix(cc_b1)
-cc_b2 <- matrix_to_brick(cc_m1)
-
-vh_m1 <- load_SAR_matrix(filename_vh)
-vh_b1 <- raster::brick(filename_vh)
-vh_m2 <- brick_to_matrix(vh_b1)
-vh_b2 <- matrix_to_brick(vh_m1)
-
-m1 <- SAR_matrix()
-
 test_that("data loads and reshapes", {
+  cc_m1 <- load_SAR_matrix(filename_cc)
+  cc_b1 <- raster::brick(filename_cc)
+  cc_m2 <- brick_to_matrix(cc_b1)
+  cc_b2 <- matrix_to_brick(cc_m1)
+
+  vh_m1 <- load_SAR_matrix(filename_vh)
+  vh_b1 <- raster::brick(filename_vh)
+  vh_m2 <- brick_to_matrix(vh_b1)
+  vh_b2 <- matrix_to_brick(vh_m1)
+
+  m1 <- SAR_matrix()
+
   expect_equal(dim(cc_m1), dim(cc_m2))
   expect_equal(dim(cc_b1), dim(cc_b2))
   expect_equal(dim(vh_m1), dim(vh_m2))
